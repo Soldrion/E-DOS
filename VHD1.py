e33365647c31c8b2aa99269ca4a45f2a0ef6d7c8
Soldrion <78869992+Soldrion@users.noreply.github.com>
2023-12-11 11:05:43 -0500

    Update VHD1.py
    Signed-off-by: Soldrion <78869992+Soldrion@users.noreply.github.com>

--- VHD1.py
+++ VHD1.py
@@ -2,4 +2,4 @@ import VHD_Folder
 from VHD_Folder import config_user
 from VHD_Folder import VHD_config
 current_vhd = config_user.contents
-vhd_config = VHD_config.contents
+vhd_config = VHD_config.desc
