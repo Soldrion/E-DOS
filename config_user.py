632387336b432d4250b118675b94a54777d09fdb
Soldrion <78869992+Soldrion@users.noreply.github.com>
2023-12-11 11:06:43 -0500

    Create config_user.py
    Signed-off-by: Soldrion <78869992+Soldrion@users.noreply.github.com>

---
+++ config_user.py
@@ -0,0 +1,10 @@
+#place inside VHD_Folder
+contents = {
+    "config_user": {
+        "hostname":"EdPc",
+        "username":"chudbuddy",
+        "password":"password",
+        "admin":False,
+        "can_use_sudo":True
+    }
+}
