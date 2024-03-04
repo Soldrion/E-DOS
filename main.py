0aa16e2a6657c38f06b25042fc68a9b22562a30c
Soldrion <78869992+Soldrion@users.noreply.github.com>
2024-03-04 10:38:58 -0500

    Update main.py
    Signed-off-by: Soldrion <78869992+Soldrion@users.noreply.github.com>

--- main.py
+++ main.py
@@ -121,12 +121,12 @@ def cmd_processor(cmd):
         for i in range(0,len(directories)):
             item = directories[i] 
             items.append(item)
-            if i == 8:
+            if i == 14:
                 items.append(" ")
                 screen_lines.append(items)
                 items = []
                 screen_lines.append(items)
-            if i == 16:
+            if i == 34:
                 items.append(" ")
                 screen_lines.append(items)
                 items = []
