e9589e3979df3e2750fdee770f537a37bbee2818
Soldrion <78869992+Soldrion@users.noreply.github.com>
2024-03-08 09:05:52 -0500

    Update main.py
    Signed-off-by: Soldrion <78869992+Soldrion@users.noreply.github.com>

--- main.py
+++ main.py
@@ -114,10 +114,9 @@ def cmd_processor(cmd):
     screen_lines.append(comb_line)
     cmd_split = cmd.split(' ')
     if cmd_split[0] == "sudo":
-        if cmd_split[1] == "dos_fdisk":
+        if cmd_split[1] == "dos_fdisk"
             screen_lines.append("the disk is f")
         if cmd_split[1] == "ls":
-            if cmd == "ls":
             screen_lines.append(" ")
             for i in range(0,len(directories)):
                 item = directories[i] 
