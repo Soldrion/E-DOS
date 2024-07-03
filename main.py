48da879b3e72c4f93dbac0ce6127a3eb90136145
Soldrion <78869992+Soldrion@users.noreply.github.com>
2024-07-03 16:27:28 -0400

    added clear
    Signed-off-by: Soldrion <78869992+Soldrion@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#remember to add the login system
-
 from brython_graphics import *
 from browser import timer
 cursor = Rectangle(10, 20)
@@ -15,9 +13,8 @@ kernel_info = "0.0.1"
 dosmode = False
 global current_directory
 current_directory = ""
-
 #DIRECTORIES
-dir_home = ["/home","sussy","baka"]
+dir_home = ["/home"]
 dir_lib = ["/lib"]
 dir_bin = ["/bin"]
 dir_tmp = ["/tmp"]
@@ -37,7 +34,7 @@ accepted_directories = [dir_home,dir_lib,dir_bin,dir_tmp,dir_etc,dir_root,dir_mn
 
 
 flash_cursor_bool = False
-command_list = ["ls","cd","pwd","mkdir","grep","exit","head","less","mv","sudo","tail","clear","dos_fdisk","dos_type","dos_edit","print","dir"]
+command_list = ["ls","cd","pwd","mkdir","grep","exit","head","less","mv","sudo","tail","clear","dos_fdisk","dos_type","dos_edit","print","dir","man"]
 
 
 screen_lines = ["Welcome to E-DOS","Kernel Version:"+kernel_info,"Storage Devices:"+str(storage_devices)]
@@ -97,13 +94,14 @@ def input_handler(e):
     global command
     global flash_cursor_bool
     flash_cursor_bool = True
-    if e.key not in ["ArrowLeft", "ArrowRight", "ArrowDown", "ArrowUp", "Control", "Alt", "Enter", "CapsLock", "Tab", "Shift","Backspace", "Meta","F1","F2","F3","F4","F5","F6","F7","F8","F9","F10","F11","F12"]:
+    if e.key not in ["ArrowLeft", "ArrowRight", "ArrowDown", "ArrowUp", "Control", "Alt", "Enter", "CapsLock", "Tab", "Shift","Backspace", "Meta"]:
         command += str(e.key)
     if e.key == "Backspace":
         command = command[:-1] 
     if e.key == "Enter":
         cmd_processor(command)
-        command = command[:-9999]         
+        command = command[:-9999] 
+        
         
         
         # Control, Shift, CapsLock, Tab, `, Escape, Backspace, Enter, Alt 
@@ -113,20 +111,6 @@ def cmd_processor(cmd):
     comb_line = username + "@" + hostname + current_directory+":" + command
     screen_lines.append(comb_line)
     cmd_split = cmd.split(' ')
-    if cmd_split[0] == "sudo":
-        if cmd_split[1] == "dos_fdisk":
-            screen_lines.append("the disk is f")
-        if cmd_split[1] == "ls":
-            screen_lines.append(" ")
-            for i in range(0,len(directories)):
-                item = directories[i] 
-                items.append(item)
-                if i % 9 == 0:
-                    items.append(" ")
-                    screen_lines.append(items)
-                    items = []
-
-        screen_lines.append(" ")
     if cmd == "dos_fdisk":
         screen_lines.append("the disk is f")
     items = []
@@ -135,20 +119,123 @@ def cmd_processor(cmd):
         for i in range(0,len(directories)):
             item = directories[i] 
             items.append(item)
-            if i % 9 == 0:
+            if i == 8:
                 items.append(" ")
                 screen_lines.append(items)
                 items = []
-
+                screen_lines.append(items)
+            if i == 16:
+                items.append(" ")
+                screen_lines.append(items)
+                items = []
+                screen_lines.append(items)
         screen_lines.append(" ")
     if cmd == "neofetch":
         screen_lines.append("package 'neofetch' not available")
 
     if cmd == "dir":
         screen_lines.append(directories)
-    if cmd == "fwd":
-        print()
-    
+    if cmd == "clear":
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
+        screen_lines.append(" ")
     if cmd_split[0] == "man":
         items = []
         screen_lines.append(" ")
@@ -190,8 +277,14 @@ def cmd_processor(cmd):
         else:
             screen_lines.append(f"Directory '{target_directory}' not found")
 
-    
-    if cmd_split[0] not in command_list and cmd_split[0] == not "sudo":
+    if cmd_split[0] == "print":
+        if cmd_split[1] == "accepted_directories":
+            screen_lines.append(accepted_directories)
+        elif cmd_split[1] == "cd":
+            screen_lines.append(current_directory)
+        else:
+            screen_lines.append(cmd_split[1])
+    if cmd_split[0] not in command_list:
         screen_lines.append("Command not found.")
         return
 
@@ -204,6 +297,7 @@ def flash_cursor():
     global flash_cursor_bool
     flash_cursor_bool = not flash_cursor_bool
 timer.set_interval(flash_cursor, 500)
+
 kernel()
 add_key_down_handler(input_handler)
 add_key_down_handler(input_callback)
